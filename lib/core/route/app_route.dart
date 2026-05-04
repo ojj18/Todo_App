36104406fa515e1ce31cbd34610ee95bdeff2321
ojj18 <manojojj511@gmail.com>
2026-05-04 21:16:25 +0530

    implementing the splash screen.

--- lib/core/route/app_route.dart
+++ lib/core/route/app_route.dart
@@ -1,12 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:todo_app/core/route/route_name.dart';
 
+import '../../features/splash/splash_screen.dart';
+
 final GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: "/",
       name: RouteName.splashScreen,
-      builder: (context, state) => const splashScreen(),
+      builder: (context, state) => const SplashScreen(),
     ),
   ],
 );
---
+++ lib/core/utils/app_string.dart
@@ -0,0 +1,39 @@
+class AppString {
+  static const appName = "Todo App";
+  static const email = "Email";
+  static const name = "Name";
+  static const password = "Password";
+  static const required = "Required";
+  static const firstName = "First Name";
+  static const lastName = "Last Name";
+  static const provideValidName = "Provide valid name";
+  static const provideValidEmail = "Provide valid email";
+  static const provideValidpPassword = "Provide valid password";
+  static const register = "Register";
+
+  static const internalServerError = "Internal server error";
+  static const internalNotFound = "Internal not found";
+  static const login = "Login";
+  static const accountCreated = "Account created";
+
+  static const searchByName = "Search by name";
+  static const newUser = "Don't have an account? ";
+  static const noDataFound = "No data found";
+
+  static const todo = "Todo list";
+
+  static const addTodo = "Add todo";
+  static const editTodo = "Edit todo";
+  static const title = "Title";
+  static const description = "Description";
+  static const add = "Add";
+  static const update = "Update";
+  static const todoCreated = "Todo created successfully";
+  static const todoUpdated = "Todo updated successfully";
+  static const todoDeleted = "Todo deleted successfully";
+  static const deleteTodo = "Delete Todo";
+  static const areYousureToDeleteTodo = "Are you sure to delete todo?";
+  static const profile = "Profile";
+  static const logout = "Logout";
+  static const profileUpdated = "Profile Updated";
+}
